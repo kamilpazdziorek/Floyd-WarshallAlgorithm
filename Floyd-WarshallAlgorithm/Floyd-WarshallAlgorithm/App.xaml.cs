ea5ac2afcd808d0df5421793e34ad61777626dbe
kamilpazdziorek <Haslo1haslo2haslo3!>
2019-02-11 20:21:08 +0100

    create project

---
+++ Floyd-WarshallAlgorithm/Floyd-WarshallAlgorithm/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Floyd_WarshallAlgorithm
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
